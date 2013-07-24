15af4e019b19827b2c3e9d9e21529be189dd8ca2
kpaskov <kpaskov@stanford.edu>
2013-07-24 15:33:19 -0700

    First commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+#SGD Website Project
+
+This project is a set of scripts used for the SGD Nextgen Redesign. It reads data from the BUD schema,
+transforms it into SPROUT objects, and then inserts it into the SPROUT database.
---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    ]
+
+tests_require = [
+    'bud_orm',
+    'sprout_orm',
+    'pyramid',
+    'waitress'
+]
+
+setup(name='schema_conversion',
+      version='0.0',
+      description='SchemaConversion',
+      long_description=README + '\n\n' + CHANGES,
+      author='Kelley Paskov',
+      author_email='kpaskov@stanford.edu',
+      url='',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = schema_conversion:main
+      """,
+      )
